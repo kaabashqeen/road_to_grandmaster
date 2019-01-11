6fa42c489666f59d5dfcc4e7a255805ac3ff6b57
Kaab Ashqeen <kaabashqeen@Kaabs-MBP.fios-router.home>
2019-01-11 03:34:46 -0600

    chat command stockfish setup working

--- screenshot_chess_website.py
+++ screenshot_chess_website.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 options = webdriver.ChromeOptions()
@@ -7,7 +8,9 @@ options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 
 options.executable_path='/usr/local/bin/chromedriver'
-# driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
+
+'''
 driver2 = webdriver.Chrome(options=options)
 
 # Login with real
@@ -21,13 +24,10 @@ driver2.find_element_by_xpath('//*[@id="login"]').click()
 
 # Play Real Game
 driver2.get('https://www.chess.com/live')
-time.sleep(5)
-chat = driver2.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input')
-chat.text = "/play"
-chat.submit()
-# driver2.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input').submit()
+chat2 = driver2.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input')
+chat2.send_keys("/play")
+chat2.send_keys(Keys.ENTER)
 
-'''
 # Login with fake
 driver.get('https://www.chess.com/')
 driver.find_element_by_xpath('/html/body/div[1]/div/div[3]/div/div[1]/a').click()
@@ -40,19 +40,20 @@ driver.find_element_by_xpath('//*[@id="login"]').click()
 # Play with fake
 driver.get('https://www.chess.com/live')
 # current_url = driver.current_url
-driver.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input').text = '/play stockfish 30 w u'
-driver.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input').submit()
+chat = driver.find_element_by_xpath('//*[@id="live-app"]/div[2]/div[2]/div/div[2]/input')
+chat.send_keys("/play stockfish 30 w u")
+chat.send_keys(Keys.ENTER)
 '''
 
-
 # bot
-# driver.get('https://www.chess.com/play/computer')
-# driver.save_screenshot("screenshot.png")
-# driver.find_element_by_xpath('//*[@id="newbie-modal"]/div/div/button').click()
-# driver.find_element_by_xpath('//*[@id="topPlayer"]/div[2]/a/span').click()
-# driver.find_element_by_xpath('//*[@id="game-settings"]/div[2]/div[1]/div[1]/div[1]/ul/li[10]/a').click()
-# driver.find_element_by_xpath('//*[@id="game-settings"]/div[1]/button/i').click()
-# driver.find_element_by_xpath('//*[@id="game-settings"]/div[1]/button').click()
+driver.get('https://www.chess.com/play/computer')
+driver.save_screenshot("screenshot.png")
+driver.find_element_by_xpath('//*[@id="newbie-modal"]/div/div/button').click()
+driver.find_element_by_xpath('//*[@id="topPlayer"]/div[2]/a/span').click()
+driver.find_element_by_xpath('//*[@id="game-settings"]/div[2]/div[1]/div[1]/div[1]/ul/li[10]/a').click()
+driver.find_element_by_xpath('//*[@id="game-settings"]/div[1]/button/i').click()
+driver.find_element_by_xpath('//*[@id="game-settings"]/div[1]/button').click()
+
 # driver.find_element_by_id('movelist_1')
 
 #
